05ae8955a6f63cc4d766588fc174b56f63ead893
Edwin Skidmore <edwin@cyverse.org>
2021-03-20 23:38:34 -0700

    Create app.R

---
+++ shiny-app/app.R
@@ -0,0 +1,71 @@
+#!/usr/bin/env Rscript
+
+# This example comes from the r-shiny examples github repo.
+# https://github.com/rstudio/shiny-examples/blob/master/001-hello/app.R
+
+library(shiny)
+
+# Define UI for app that draws a histogram ----
+ui <- fluidPage(
+
+  # App title ----
+  titlePanel("Hello Shiny!"),
+
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+
+      # Input: Slider for the number of bins ----
+      sliderInput(inputId = "bins",
+                  label = "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+
+    ),
+
+    # Main panel for displaying outputs ----
+    mainPanel(
+
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
+
+    )
+  )
+)
+
+
+# Define server logic required to draw a histogram ----
+server <- function(input, output) {
+
+  # Histogram of the Old Faithful Geyser Data ----
+  # with requested number of bins
+  # This expression that generates a histogram is wrapped in a call
+  # to renderPlot to indicate that:
+  #
+  # 1. It is "reactive" and therefore should be automatically
+  #    re-executed when inputs (input$bins) change
+  # 2. Its output type is a plot
+  output$distPlot <- renderPlot({
+
+    x    <- faithful$waiting
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    hist(x, breaks = bins, col = "#75AADB", border = "white",
+         xlab = "Waiting time to next eruption (in mins)",
+         main = "Histogram of waiting times")
+
+    })
+
+}
+
+# If you want to automatically reload the app when your codebase changes - should be turned off in production
+options(shiny.autoreload = TRUE)
+
+options(shiny.host = '0.0.0.0')
+options(shiny.port = 8080)
+
+# Create Shiny app ---- 
+shinyApp(ui = ui, server = server)
